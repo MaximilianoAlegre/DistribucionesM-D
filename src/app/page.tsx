17d3fc5f29a0b7cd67fa31f232afe2aae4756d94
LautaroJLZ <lautarojoseluis1234@gmail.com>
2024-06-15 15:14:23 -0300

    Middleware de mierda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { NewProducts } from "@/components";
+import { NewProducts, Welcome } from "@/components";
 
 export default function Home() {
   return (
     <main>
+      <Welcome/>
       <NewProducts/>
     </main>
   );
